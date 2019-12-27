b87ca8b01490e429bab9410bc283a424fa6c6e29
Nawar <nawarmk98@gmail.com>
2019-12-27 11:11:03 +0200

    first commit

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/categories', function () {
+    return \App\Category::all();
+});
+
+
 
 Route::get('/', function () {
     return view('welcome');
